bf093b0ed596b546e9c114c65844e78065528abd
Sebastian Mayor Molinares <smayor@redsis.com>
2017-10-13 18:50:13 -0500

    started second part

---
+++ udemySecondApp/js/app.js
@@ -0,0 +1,11 @@
+new Vue({
+  el: '#app',
+  data: {
+    show: true,
+    ingredients: ['meat', 'fruit', 'cookie'],
+    persons: [
+    {name: 'Max', age: 27, color: 'red'},
+    {name: 'Ana', age: 'unknown', color: 'blue'},
+    ]
+  }
+})
