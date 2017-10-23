74583acd22e16878ba22a8092e1334286f7366ac
Sebastian Mayor Molinares <smayor@redsis.com>
2017-10-23 18:01:29 -0500

    components basics

---
+++ Section Code (Start)/src/ServerStatus.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p> Server Status: {{status}} </p>
+    <hr>
+    <button @click="changeStatus"> changeStatus</button>
+  </div>
+</template>
+
+<script>
+  export default{
+    data: function(){
+      return {
+        status : 'critical'
+      }
+    },
+
+    methods: {
+      changeStatus(){
+        this.status = 'Normal'
+      }
+    }
+  }
+</script>
---
+++ VueCliAndComponents/Start/src/Main.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1> Esto es Many Que!</h1>
+    <ready-stuff></ready-stuff>
+  </div>
+</template>
