512e41ec1900cc6561ed7adc14adec940f3e0134
Sebastian Mayor Molinares <smayor@redsis.com>
2017-10-30 18:11:16 -0500

    Add Section 9 (more components) and WonderFull Quotes App

---
+++ (10) Wonderful Quotes/Start/src/components/QuoteGrid.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <app-quote v-for="(quote, index) in quotes" @click.native="deleteQuote(index)"> {{ quote }} </app-quote>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Quote from './Quote.vue'
+  export default{
+    props: ['quotes'],
+    components:{
+      appQuote: Quote
+    },
+    methods: {
+      deleteQuote(index){
+        this.$emit('quoteDeleted', index)
+      }
+    }
+  }
+</script>
+
+<style>
+  
+</style>
